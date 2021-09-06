051e70f4e35491b17929c30006b9c5618d6c1787
Pavel Fedorov <fedorovpa@gmail.com>
2021-09-05 21:16:59 -0700

    Added main file.

---
+++ main.lua
@@ -0,0 +1,15 @@
+function love.load()
+	x = 100
+end
+
+function love.draw()
+    love.graphics.polygon("fill", x,100, x+100,100, x+50,200)
+end
+
+function love.update(dt)
+	if love.keyboard.isDown("left") then
+        x = x - 5 * dt
+    elseif love.keyboard.isDown("right") then
+        x = x + 5 * dt
+    end
+end
